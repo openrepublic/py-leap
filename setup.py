9c7ad384df53ab08b79853014b87f909a0af31d5
Guillermo Rodriguez <guillermo@telos.net>
2022-09-09 19:52:07 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 	name='py-eosio',
-	version='0.1a7',
+	version='0.1a8',
 	author='Guillermo Rodriguez',
 	author_email='guillermor@fing.edu.uy',
 	packages=['py_eosio'],
