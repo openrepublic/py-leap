9edcc0a805bb835b23f92329fbd8bb017c270ab8
Guillermo Rodriguez <guillermo@telos.net>
2025-03-20 23:26:57 -0300

    Drop pytest-trio, its giving problems on win & mac ci

--- tests/test_ship.py
+++ tests/test_ship.py
@@ -1,5 +1,6 @@
 import json
 
+import trio
 import pytest
 
 from leap.ship import open_state_history
@@ -19,7 +20,7 @@ from leap.ship.structs import OutputFormats
         'general_case_standard'
     ]
 )
-async def test_one_tx(
+def test_one_tx(
     cleos_bs,
     fetch_block: bool,
     fetch_traces: bool,
@@ -37,24 +38,27 @@ async def test_one_tx(
     tx_block_num = receipt['processed']['block_num']
 
     blocks = []
-    async with open_state_history(
-        sh_args={
-            'endpoint': cleos.ship_endpoint,
-            'start_block_num': tx_block_num,
-            'end_block_num': tx_block_num + 1,
-            'fetch_block': fetch_block,
-            'fetch_traces': fetch_traces,
-            'fetch_deltas': fetch_deltas,
-            'start_contracts': start_contracts,
-            'action_whitelist': action_whitelist,
-            'delta_whitelist': delta_whitelist,
-            'decode_meta': decode_meta,
-            'output_format': output_format
-        }
-    ) as rchan:
-        async for block in rchan:
-            print(json.dumps(block.as_dict(), indent=4, cls=LeapJSONEncoder))
-            blocks.append(block)
+    async def main():
+        async with open_state_history(
+            sh_args={
+                'endpoint': cleos.ship_endpoint,
+                'start_block_num': tx_block_num,
+                'end_block_num': tx_block_num + 1,
+                'fetch_block': fetch_block,
+                'fetch_traces': fetch_traces,
+                'fetch_deltas': fetch_deltas,
+                'start_contracts': start_contracts,
+                'action_whitelist': action_whitelist,
+                'delta_whitelist': delta_whitelist,
+                'decode_meta': decode_meta,
+                'output_format': output_format
+            }
+        ) as rchan:
+            async for block in rchan:
+                print(json.dumps(block.as_dict(), indent=4, cls=LeapJSONEncoder))
+                blocks.append(block)
+
+    trio.run(main)
 
     block = blocks[0]
 
--- tests/test_table_struct.py
+++ tests/test_table_struct.py
@@ -1,32 +1,35 @@
+import trio
 from msgspec import Struct
 
 from leap import CLEOS
 
 
-async def test_account_balance():
+def test_account_balance():
 
-    class BalanceRow(Struct):
-        balance: str
+    async def main():
+        class BalanceRow(Struct):
+            balance: str
 
-    cleos = CLEOS('https://testnet.telos.net')
+        cleos = CLEOS('https://testnet.telos.net')
 
-    res = cleos.get_table(
-        'eosio.token', 'eosio', 'accounts')
-    assert len(res) > 0
+        res = cleos.get_table(
+            'eosio.token', 'eosio', 'accounts')
+        assert len(res) > 0
 
-    sres = cleos.get_table(
-        'eosio.token', 'eosio', 'accounts', resp_cls=BalanceRow)
-    assert len(sres) > 0
+        sres = cleos.get_table(
+            'eosio.token', 'eosio', 'accounts', resp_cls=BalanceRow)
+        assert len(sres) > 0
 
-    assert res[0]['balance'] == sres[0].balance
+        assert res[0]['balance'] == sres[0].balance
 
-    res = await cleos.aget_table(
-        'eosio.token', 'eosio', 'accounts')
-    assert len(res) > 0
+        res = await cleos.aget_table(
+            'eosio.token', 'eosio', 'accounts')
+        assert len(res) > 0
 
-    sres = await cleos.aget_table(
-        'eosio.token', 'eosio', 'accounts', resp_cls=BalanceRow)
-    assert len(sres) > 0
+        sres = await cleos.aget_table(
+            'eosio.token', 'eosio', 'accounts', resp_cls=BalanceRow)
+        assert len(sres) > 0
 
-    assert res[0]['balance'] == sres[0].balance
+        assert res[0]['balance'] == sres[0].balance
 
+    trio.run(main)
